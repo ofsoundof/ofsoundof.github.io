d94e9e56e5e962815361d94d2178c5ce19ed971f
ofsoundof <li.yawei.ai@gmail.com>
2025-10-01 20:14:35 +0000

    Deploying to gh-pages from @ ofsoundof/ofsoundof.github.io@d63cd0bd7a36af9d061333bb5210a6b85a006cfe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/talks/";
+          },
         },{id: "nav-service",
           title: "Service",
           description: "",
